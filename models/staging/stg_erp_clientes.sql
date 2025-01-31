17a6a48603e09499aaacffa2c6d1b24479bcb0bb
LEANDRO LACHINI NASCIMENTO <controler@lnlogistica.com.br>
2025-01-31 19:28:48 +0000

    Finalizando tabela fato propostas.

--- models/staging/stg_erp_clientes.sql
+++ models/staging/stg_erp_clientes.sql
@@ -12,15 +12,14 @@ with
     , remane_table as (
         select
             cast(COD_CLIENTE as int) as COD_CLIENTE
+            , cast(SUBSTRING(DATA_INCLUSAO, 1, 10) as date) as DATA_INCLUSAO_CLIENTE
+            , cast(DATA_NASCIMENTO as date) as DATA_NASCIMENTO_CLIENTE
             , cast(PRIMEIRO_NOME as varchar) as PRIMEIRO_NOME
             , cast(ULTIMO_NOME as varchar) as ULTIMO_NOME
             , PRIMEIRO_NOME || ' ' || ULTIMO_NOME as NOME_COMPLETO_CLIENTE
             , cast(EMAIL as varchar) as EMAIL_CLIENTE
             , cast(TIPO_CLIENTE as varchar) as TIPO_CLIENTE
-            --, cast(DATA_INCLUSAO as varchar) as DATA_INCLUSAO_CLIENTE
-            , cast(SUBSTRING(DATA_INCLUSAO, 1, 10) as date) as DATA_INCLUSAO_CLIENTE
             , cast(CPFCNPJ as varchar) as CPFCNPJ_CLIENTE
-            , cast(DATA_NASCIMENTO as date) as DATA_NASCIMENTO_CLIENTE
             , cast(ENDERECO as varchar) as ENDERECO_CLIENTE
             , cast(CEP as varchar) as CEP_CLIENTE
         from source
