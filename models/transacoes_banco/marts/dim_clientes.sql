b3fa8e940ac8daa67beb34625b3429886319230c
LEANDRO LACHINI NASCIMENTO <controler@lnlogistica.com.br>
2025-02-01 16:56:26 +0000

    Inserido metricas na dimensao clientes.

--- models/transacoes_banco/marts/dim_clientes.sql
+++ models/transacoes_banco/marts/dim_clientes.sql
@@ -4,17 +4,12 @@ with
     clientes as (
         select
             COD_CLIENTE
-            , PRIMEIRO_NOME
-            , ULTIMO_NOME
             , NOME_COMPLETO_CLIENTE
-            , EMAIL_CLIENTE
             , TIPO_CLIENTE
             , DATA_INCLUSAO_CLIENTE
-            , CPFCNPJ_CLIENTE
-            , DATA_NASCIMENTO_CLIENTE
-            , ENDERECO_CLIENTE
-            , CEP_CLIENTE
-        from {{ref("stg_erp_clientes")}}
+            , IDADE_CLIENTE
+            , FAIXA_ETARIA
+        from {{ref("int_clientes")}}
     )
 
 select * from clientes
